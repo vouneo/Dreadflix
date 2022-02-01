912045312f6f53aa4954e63f4baf1318045a13af
Octavio García <octavio.garcia@gmx.com>
2022-02-01 20:37:38 -0300

    Primeras implementaciones en el CRUD de express.js

---
+++ utils/mock/movies.js
@@ -0,0 +1,148 @@
+const moviesMock = [{
+    "id": "4d484c8a-7b94-4c91-82d7-b411b3c3107f",
+    "title": "Dirty Wars",
+    "year": 1997,
+    "cover": "http://dummyimage.com/141x126.png/cc0000/ffffff",
+    "description": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+    "duration": 1954,
+    "contentRating": "NC-17",
+    "source": "http://washingtonpost.com/magna.html",
+    "tags": [
+      "Action|Adventure|Thriller",
+      "Drama",
+      "Action|Comedy|Crime",
+      "Drama|Mystery",
+      "Documentary"
+    ]
+  }, {
+    "id": "47374a7c-86a7-4b85-b504-99dc86bcd384",
+    "title": "Bandit, The (Eskiya)",
+    "year": 2004,
+    "cover": "http://dummyimage.com/235x150.png/5fa2dd/ffffff",
+    "description": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+    "duration": 1910,
+    "contentRating": "R",
+    "source": "http://nyu.edu/amet/sapien/dignissim/vestibulum/vestibulum.jpg",
+    "tags": [
+      "Action|Crime|Thriller",
+      "Adventure|Fantasy",
+      "Drama",
+      "Sci-Fi",
+      "Adventure|Documentary"
+    ]
+  }, {
+    "id": "2d49ac0c-9c55-425e-bec3-0a15e639af70",
+    "title": "Cutie Honey",
+    "year": 1993,
+    "cover": "http://dummyimage.com/146x243.png/cc0000/ffffff",
+    "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+    "duration": 1988,
+    "contentRating": "PG",
+    "source": "http://geocities.jp/adipiscing/elit/proin/risus/praesent.html",
+    "tags": [
+      "Comedy|Drama",
+      "Drama",
+      "Comedy|Drama",
+      "Sci-Fi"
+    ]
+  }, {
+    "id": "5640f6a5-9e0f-4db9-87ac-105e765d8a20",
+    "title": "Woman of Affairs, A",
+    "year": 1995,
+    "cover": "http://dummyimage.com/206x227.png/ff4444/ffffff",
+    "description": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+    "duration": 1894,
+    "contentRating": "PG",
+    "source": "https://elpais.com/venenatis.xml",
+    "tags": [
+      "Drama",
+      "Comedy|Drama|Romance"
+    ]
+  }, {
+    "id": "4e50dc96-479b-4a33-9dc4-aa9ede7eebf8",
+    "title": "National Lampoon's Van Wilder",
+    "year": 2008,
+    "cover": "http://dummyimage.com/231x150.png/dddddd/000000",
+    "description": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
+    "duration": 2057,
+    "contentRating": "PG-13",
+    "source": "https://ning.com/in.json",
+    "tags": [
+      "Comedy",
+      "Action|Adventure|Mystery|Sci-Fi",
+      "Drama"
+    ]
+  }, {
+    "id": "1a01e7e0-07c1-4cca-a194-48c9dd3b5eb1",
+    "title": "Never Back Down",
+    "year": 1994,
+    "cover": "http://dummyimage.com/172x200.png/ff4444/ffffff",
+    "description": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    "duration": 2062,
+    "contentRating": "R",
+    "source": "http://issuu.com/blandit/nam/nulla/integer.jsp",
+    "tags": [
+      "Comedy|Drama"
+    ]
+  }, {
+    "id": "5cb4b91c-6cb6-4311-8109-712fb779091a",
+    "title": "Ouch (Aïe)",
+    "year": 2008,
+    "cover": "http://dummyimage.com/179x162.png/ff4444/ffffff",
+    "description": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
+    "duration": 1917,
+    "contentRating": "G",
+    "source": "http://chron.com/et/ultrices/posuere/cubilia/curae.jsp",
+    "tags": [
+      "Drama",
+      "Crime|Mystery"
+    ]
+  }, {
+    "id": "505b4a21-5dec-44d7-b9c1-d8598bdee5f7",
+    "title": "29 Palms",
+    "year": 1999,
+    "cover": "http://dummyimage.com/127x186.png/ff4444/ffffff",
+    "description": "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+    "duration": 1946,
+    "contentRating": "PG",
+    "source": "http://yolasite.com/at/turpis/donec/posuere/metus/vitae/ipsum.png",
+    "tags": [
+      "Documentary",
+      "Action|Crime",
+      "Children|Comedy|Fantasy",
+      "Comedy|Musical",
+      "Drama|Romance|Thriller|War"
+    ]
+  }, {
+    "id": "971540b6-5c2c-4dff-9488-aa80f452de27",
+    "title": "One Tough Cop",
+    "year": 1987,
+    "cover": "http://dummyimage.com/120x127.png/cc0000/ffffff",
+    "description": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    "duration": 1976,
+    "contentRating": "PG",
+    "source": "https://gravatar.com/quam/nec/dui/luctus/rutrum/nulla/tellus.html",
+    "tags": [
+      "Comedy|Drama|Romance",
+      "Horror|Mystery|Thriller",
+      "Adventure|Comedy|Drama|Romance",
+      "Action|Adventure|Sci-Fi",
+      "Drama"
+    ]
+  }, {
+    "id": "e7fbcab2-0e61-47f8-929f-58c08a8285ab",
+    "title": "Astronaut's Wife, The",
+    "year": 2008,
+    "cover": "http://dummyimage.com/102x111.png/dddddd/000000",
+    "description": "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+    "duration": 1962,
+    "contentRating": "NC-17",
+    "source": "http://shop-pro.jp/ut/erat.js",
+    "tags": [
+      "Drama|Horror|Thriller"
+    ]
+  }];
+
+  module.exports = {
+      moviesMock
+  };
